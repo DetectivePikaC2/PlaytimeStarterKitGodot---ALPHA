ffe53143ca9339db836c70b4e6b31824a340b325
DetectivePikaC2 <166664745+DetectivePikaC2@users.noreply.github.com>
2025-02-18 14:13:45 -0600

    UNIFINISHED

--- Objects/Characters/TESTpiano/piano_saurus.gd
+++ Objects/Characters/TESTpiano/piano_saurus.gd
@@ -1,3 +1,7 @@
 extends Node3D
 
 @onready var animation_player: AnimationPlayer = $pianosaurusmodel/AnimationPlayer
+
+func step():
+	var path: String = str("Step", randi_range(1, 3))
+	get_node(path).play()
